4253ab2acbb72d05911711c0fa5aa111a4ca26e2
972046261 <972046261@qq.com>
2021-01-25 20:20:19 +0800

    第一上传文件

---
+++ demo.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', function () {
+    let box = document.getElementsByClassName('box')[0]
+    box.addEventListener('click', () => {
+        alert(这是测试)
+    })
+    console.log(1);
+})
